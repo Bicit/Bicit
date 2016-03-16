61a17bbf44243b5380997a67d3d59215370d2d26
MrPiipe <piipe.donado@hotmail.com>
2016-03-16 16:13:47 -0500

    XML del Splash Screen

---
+++ Bicit/app/src/main/java/com/bicit/bicit/bicitSplash.java
@@ -0,0 +1,15 @@
+package com.bicit.bicit;
+
+/**
+ * Created by piipe on 3/16/2016.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class bicitSplash extends Activity {
+
+
+}
