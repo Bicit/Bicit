026632a6af7f60c1473e4da1f3401396fdd9d5b7
fmontoy <fmontoyalvarez@gmail.com>
2016-04-05 11:04:03 -0500

    Fixes

--- Bicit/app/src/main/java/com/bicit/bicit/LoginActivity.java
+++ Bicit/app/src/main/java/com/bicit/bicit/LoginActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -37,17 +38,20 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(getApplicationContext(), "OnSuccess", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(LoginActivity.this,Maproute.class);
                 startActivity(intent);
             }
 
             @Override
             public void onCancel() {
+                Toast.makeText(getApplicationContext(), "OnCancel", Toast.LENGTH_SHORT).show();
 
             }
 
             @Override
             public void onError(FacebookException e) {
+                Toast.makeText(getApplicationContext(), "OnError", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -56,6 +60,12 @@ public class LoginActivity extends AppCompatActivity {
     public void onClick(View v ){
             Intent intent = new Intent(LoginActivity.this,Maproute.class);
             startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 
