e062d3b17aedfeed036a4c71c5a39303e6b6e87d
jgavirias13 <jgavirias13@gmail.com>
2016-04-04 23:37:24 -0500

    Se agrega la verificacion de permisos y se modifica el android manifest agregando la actividad de login

--- Bicit/app/src/main/java/com/bicit/bicit/Maproute.java
+++ Bicit/app/src/main/java/com/bicit/bicit/Maproute.java
@@ -1,9 +1,11 @@
 package com.bicit.bicit;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -56,6 +58,13 @@ public class Maproute extends FragmentActivity implements OnMapReadyCallback {
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
         String provider = locationManager.getBestProvider(criteria, true);
+
+        //Se revisa si se tienen los permisos para conocer la localizacion
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            return;
+        }
+
         Location location = locationManager.getLastKnownLocation(provider);
 
         Mapa controladorMapa = new Mapa(R.raw.ciclorutasmap, mMap, location, getApplicationContext());
