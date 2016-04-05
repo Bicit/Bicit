f5a6c4c4af09a3aa8260e237cc4c157b1e91a06d
fmontoy <fmontoyalvarez@gmail.com>
2016-04-05 10:21:12 -0500

    Se crea el "Login with Facebook"

--- Bicit/app/src/main/java/com/bicit/bicit/LoginActivity.java
+++ Bicit/app/src/main/java/com/bicit/bicit/LoginActivity.java
@@ -6,29 +6,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
 
-    public EditText txtUser;
-    public EditText txtPassword;
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
+
     public Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
-        this.txtUser = (EditText) findViewById(R.id.txtUser);
-        this.txtPassword = (EditText) findViewById(R.id.txtPassword);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
         this.btnLogin = (Button) findViewById(R.id.btnLogin);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent = new Intent(LoginActivity.this,Maproute.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
     }
 
     public void onClick(View v ){
-        String user = txtUser.getText().toString();
-        String password = txtPassword.getText().toString();
-        if(user.equals("") && password.equals("")){
             Intent intent = new Intent(LoginActivity.this,Maproute.class);
             startActivity(intent);
-        }
     }
 
 
