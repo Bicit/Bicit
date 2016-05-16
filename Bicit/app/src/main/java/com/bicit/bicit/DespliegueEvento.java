6690c10d8148d93c5a18f82fc1707ad256fc5f32
fmontoy <fmontoyalvarez@gmail.com>
2016-05-16 13:07:00 -0500

    se crea actividad de despliegue de eventos

---
+++ Bicit/app/src/main/java/com/bicit/bicit/DespliegueEvento.java
@@ -0,0 +1,13 @@
+package com.bicit.bicit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DespliegueEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_despliegue_evento);
+    }
+}
